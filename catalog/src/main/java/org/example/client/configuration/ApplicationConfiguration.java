b1392dd765ab672f36ba3825a0460899a9b669e2
amirxanrt <amirxanrt@hotmail.com>
2022-08-19 14:13:48 +0300

    setting up the database

--- catalog/src/main/java/org/example/client/configuration/ApplicationConfiguration.java
+++ catalog/src/main/java/org/example/client/configuration/ApplicationConfiguration.java
@@ -1,4 +1,4 @@
-package org.example.configuration;
+package org.example.client.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
