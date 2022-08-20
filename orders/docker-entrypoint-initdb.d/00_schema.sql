b69655c46a4d10cbaf5955ef9a7cdd2c4ec0aae6
RatnikovMaxim <maxim.ratnikov@mail.ru>
2022-08-20 11:24:17 +0300

    added orders

---
+++ orders/docker-entrypoint-initdb.d/00_schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE orders
+(
+    id       BIGSERIAL PRIMARY KEY,
+    owner_id BIGINT      NOT NULL /*из users*/,
+    sto_id   BIGINT      NOT NULL /*из Catalog*/,
+    service_id  BIGINT      NOT NUll /*из services*/,
+    type     TEXT        NOT NULL, /* "заброннирован", "в работе", "выполнен" */
+    removed  BOOLEAN     NOT NULL DEFAULT FALSE,
+    created  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE history
+(
+    id            BIGSERIAL PRIMARY KEY,
+    order_number  BIGINT      NOT NULL REFERENCES orders,
+    owner_id      BIGINT      NOT NULL /*из users*/,
+    sto_id        BIGINT      NOT NULL /*из Catalog*/,
+    service_id      BIGINT      NOT NUll /*из services*/,
+    type          TEXT        NOT NULL, /* "выполнен" */
+    order_created timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
