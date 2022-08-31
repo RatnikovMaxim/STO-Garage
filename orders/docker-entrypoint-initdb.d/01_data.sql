dae7ce411c78699e108bd3f66d16380b0ebace51
RatnikovMaxim <maxim.ratnikov@mail.ru>
2022-08-31 22:27:02 +0300

    changed any

--- orders/docker-entrypoint-initdb.d/01_data.sql
+++ orders/docker-entrypoint-initdb.d/01_data.sql
@@ -1,9 +1,14 @@
 INSERT INTO orders (user_id, user_name, station_id, station_name, status)
-VALUES (1, 'Vasiliy', 2, 'Авто Доктор', 'заброннирован');
+VALUES (1, 'Vasiliy', 1, 'Авто Доктор', 'заброннирован'),
+       (1, 'Vasiliy', 2, 'Мир Машин', 'заброннирован');
 
 
 INSERT INTO order_positions (order_id, service_id, service_name, price)
-VALUES (1, 1, 'Шиномонтаж', 1500);
+VALUES (1, 1, 'Шиномонтаж', 1500),
+       (1, 2, 'ТехОбслуживание', 2000),
+       (1, 3, 'Кузовные работы', 2500),
+       (2, 4, 'Эвакуатор', 1200),
+       (2, 5, 'Слесарные работы', 1700);
 
 
 
